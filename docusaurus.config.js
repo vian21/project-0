8f5196466bf8dd95fee93fc5ec1abebd250e1024
vian21 <pigir101@uotawa.ca>
2022-10-25 12:59:25 -0400

    chnaged favi icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/project-0/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
