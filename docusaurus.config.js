ad9f3a1e6cc69044a2dfe93087b84c1c9b79d588
vian21 <pigir101@uotawa.ca>
2022-10-25 14:21:04 -0400

    deployement

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   organizationName: 'vian21', // Usually your GitHub org/user name.
   projectName: 'project-0', // Usually your repo name.
   deploymentBranch: "gh-pages",
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
